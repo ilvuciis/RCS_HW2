51376045fba25d45697f0d2970ea1c8bed7229c7
Ilva Aleksandrova <ilvuciis@gmail.com>
2019-05-28 16:25:20 +0300

    Final touches

--- scripts/first.js
+++ scripts/first.js
@@ -1,1 +1,12 @@
-alert("Hello RCS");
+let greeting = "Super Long"
++ "Greeting"
++ 5*5;
+globalVar = 6;
+console.log("My body is " + document.body.clientWidth + " wide");
+if (document.body.clientWidth >600) {
+document.body.style.backgroundColor = "green";}
+else {
+    document.body.style.backgroundColor = "orange";
+}
+
+
